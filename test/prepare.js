af3ef5e6e876b3cb72c2c90b0b8208af4b921093
Cemre MENGU <cemre.mengu@ttgint.com>
2018-04-13 15:36:30 +0300

    Initial pg config

--- test/prepare.js
+++ test/prepare.js
@@ -4,4 +4,9 @@ const execSync = require('child_process').execSync;
 
 execSync('mysql -h 127.0.0.1 -uroot -e "create database IF NOT EXISTS test;"');
 execSync('mysql -h 127.0.0.1 -uroot test < test/npm_auth.sql');
-console.log('create table success');
+console.log('init mysql success');
+
+execSync('psql  -h 127.0.0.1 -U postgres -c "drop database IF EXISTS test;"');
+execSync('psql  -h 127.0.0.1 -U postgres -c "create database test;"');
+execSync('psql  -h 127.0.0.1 -U postgres -d test -a -f test/npm_auth_pg.sql');
+console.log('init postgresql success');
