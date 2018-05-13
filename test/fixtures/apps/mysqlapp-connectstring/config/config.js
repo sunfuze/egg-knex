d00e40c7ea44bc7033d15bbffc76813d5667721b
Cemre MENGU <cemre.mengu@ttgint.com>
2018-05-13 15:18:44 +0300

    Added tests for connectString

---
+++ test/fixtures/apps/mysqlapp-connectstring/config/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+exports.keys = "i7oBLQc43aiDCC9zxYrYNw==";
+
+exports.knex = {
+  client: {
+    dialect: "mysql",
+    connection: {
+      connectString: "Server=127.0.0.1:3306;Database=test;Uid=root;"
+    }
+  },
+  agent: true
+};
